77fbb5eb9a6b33f7139f9d55eee5f62d2fa08d05
Aarunya Rath <aarunya.rath@gmail.com>
2026-03-15 19:40:18 +0530

    UC12: Final enhanced palindrome checker

---
+++ UseCase12PalindromeCheckerApp.java
@@ -0,0 +1,62 @@
+import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+public class UseCase12PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "Level";
+
+        PalindromeStrategy strategy = new StackStrategy();
+
+        boolean result = strategy.check(input.toLowerCase());
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome?: " + result);
+    }
+}
